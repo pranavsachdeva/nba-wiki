b5b6a162b4e305bd6a3278bfe559fe50e39ffa18
pranavsachdeva <pranavsachdeva2000@gmail.com>
2020-01-12 10:13:53 -0800

    nba

---
+++ src/components/button.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import './button.css';
+import Button from 'react-bootstrap/Button'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton'
+import Games from './Games';
+import Teams from './Teams';
+
+
+class Button1 extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Router>
+            <div>
+
+            
+
+            <Button className="btngrp1" variant="outline-primary" toggle href="/teams">NBA Teams</Button>
+            <Button  className="btngrp1" variant="outline-primary" href="/games">NBA Games</Button>
+            {/* <Nav justify variant="tabs" defaultActiveKey="/home">
+            <Nav.Item>
+                <Nav.Link href="/teams">NBA Teams</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/games">NBA Games</Nav.Link>
+            </Nav.Item>
+            
+            </Nav> */}
+            </div>
+            <Route path="/teams" component={Teams}/>
+            <Route path="/games" component={Games}/>
+            
+            </Router>
+         );
+    }
+}
+ 
+export default Button1;
